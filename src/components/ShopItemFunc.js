30f65ab8183aac16828c92f6cffec479d8f62980
psdreams <63098062+psdreams@users.noreply.github.com>
2020-05-27 21:55:42 +0300

    homework func component first commit

---
+++ src/components/ShopItemFunc.js
@@ -0,0 +1,39 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const ShopItemFunc = ({ item }) => {
+  return (
+    <div className="main-content">
+      <h2>{item.brand}</h2>
+      <h1>{item.title}</h1>
+      <h3>{item.description}</h3>
+      <div className="description">{item.descriptionFull}</div>
+      <div className="highlight-window mobile">
+        <div className="highlight-overlay"></div>
+      </div>
+      <div className="divider"></div>
+      <div className="purchase-info">
+        <div className="price">
+          {item.currency}
+          {item.price.toFixed(2)}
+        </div>
+        <button>Добавить в корзину</button>
+      </div>
+    </div>
+  );
+};
+
+const userType = PropTypes.shape({
+  brand: PropTypes.string.isRequired,
+  title: PropTypes.string,
+  description: PropTypes.string,
+  descriptionFull: PropTypes.string,
+  price: PropTypes.number,
+  currency: PropTypes.string,
+});
+
+ShopItemFunc.propTypes = {
+  item: userType,
+};
+
+export default ShopItemFunc;
